3623fe53a8fef6fc0ec2619037b9be12ea7172ca
Ran_ying <62641267+Ran-ying@users.noreply.github.com>
2023-01-19 16:44:20 +0300

    update

--- n.js
+++ n.js
@@ -1,12 +1,20 @@
-require('fs').readdir('music',(err, file)=>{
+let art = require('./musicList.json')
+
+require("fs").readdir("music",(err, file)=>{
+    let url = "https://music.yir.moe/music/"
+    console.log("[")
+    let count = 0
     for(let i in file){
+        if(file[i] != ".DS_Store" && file[i].split(".")[1] != "webp")
         console.log(
-`{ 
-    name: '${file[i].split('.')[0]}', 
-    artist: '',
-    url: './music/${file[i]}',
-    cover: './music/${file[i].split('.')[0]}.webp',
-    lrc: './music/${file[i].split('.')[0]}.lrc',
+`{
+    "id": ${count++},
+    "name": "${file[i].split(".")[0]}", 
+    "artist": "${art[file[i].split(".")[0]]}",
+    "url": "${encodeURI(url+file[i])}",
+    "cover": "${encodeURI(url+file[i].split(".")[0])}.webp",
+    "lrc": "${encodeURI(url+file[i].split(".")[0])}.lrc"
 },`)
     }
+    console.log("]")
 })
