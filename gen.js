f9f384f0c1f6b01c7ec81f9dcad1da1c513990e5
Ran_ying <62641267+Ran-ying@users.noreply.github.com>
2023-01-26 16:22:27 +0300

    fix

--- gen.js
+++ gen.js
@@ -1,4 +1,4 @@
-{
+let art = {
     "千里邀月": "泠鸢yousa & hanser & 三无Marblue & 茶理理",
     "That Girl": "Olly Murs",
     "ふたりのきもちのほんとのひみつ": "やすなとソーニャ",
@@ -26,3 +26,22 @@
     "闻妖":"银临",
     "平安奇妙物语":"千月兔 & 泠鸢yousa & KBShinya"
 }
+
+require("fs").readdir("music",(err, file)=>{
+    let url = "https://music.yir.moe/music/"
+    console.log("[")
+    let count = 0
+    for(let i in file){
+        if(file[i] != ".DS_Store" && file[i].split(".")[1] != "webp")
+        console.log(
+`{
+    "id": ${count++},
+    "name": "${file[i].split(".")[0]}", 
+    "artist": "${art[file[i].split(".")[0]]}",
+    "url": "${encodeURI(url+file[i])}",
+    "cover": "${encodeURI(url+file[i].split(".")[0])}.webp",
+    "lrc": "${encodeURI(url+file[i].split(".")[0])}.lrc"
+},`)
+    }
+    console.log("]")
+})
